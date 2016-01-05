b70396e97d92f629cfadcd1a47f85152c458cc32
quangdai02031994 <quangdai02031994@gmail.com>
2016-01-05 17:08:12 +0700

    Start Project 05/01/2016

---
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera _mainCamera;
+
+
+    public float _speed;
+    public float _maxPosition;
+
+    void Start()
+    {
+        _maxPosition = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).x - 0.93f;
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x * Time.deltaTime * _speed, 0, 0);
+
+        if (transform.position.x > _maxPosition)
+        {
+            transform.position = new Vector3(_maxPosition, transform.position.y, 0);
+        }
+        else if(transform.position.x < -_maxPosition)
+        {
+            transform.position = new Vector3(-_maxPosition, transform.position.y, 0);
+        }
+        
+    }
+
+}
---
+++ Assets/_Scripts/Road/MoveOffset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOffset : MonoBehaviour {
+
+    public Renderer rend;
+
+    public float _scrollSpeed;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = _scrollSpeed * Time.time;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+
+
+
+}
